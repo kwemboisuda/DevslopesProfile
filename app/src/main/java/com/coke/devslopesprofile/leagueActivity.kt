03608f4c6da1a95be5539324e69857e503c09266
kwemboSuda <kwemboisudaiz65@gmail.com>
2023-04-03 16:11:21 +0300

    lesson 2

---
+++ app/src/main/java/com/coke/devslopesprofile/leagueActivity.kt
@@ -0,0 +1,11 @@
+package com.coke.devslopesprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class leagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
